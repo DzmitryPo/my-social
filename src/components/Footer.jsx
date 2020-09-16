7f5225cc279c2ade8284e6abdabdf305e9404510
Dmitry Po <dima.1c8.2@gmail.com>
2020-09-16 18:29:36 +0300

    add modules, footer and basic grid

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Bottom.module.css'
+
+const Bottom = () => {
+    return <bottom className={s.item}>
+        <a>BOTTOM</a>
+    </bottom>
+}
+
+export default Bottom;
